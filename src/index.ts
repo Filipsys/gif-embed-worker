bf165a4b7ea12b79d0abd249eec0faffdce45778
Filipsys <filipsysak@gmail.com>
2025-10-04 23:15:47 +0200

    Changed svg-png library

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,3 @@
-import sharp from "sharp";
-
 export default {
 	async fetch(request, env, ctx): Promise<Response> {
 		let { pathname } = new URL(request.url);
@@ -9,7 +7,7 @@ export default {
 			const list = await env.IMAGES.list();
 
 			const svg = `
-				<svg width="800" height="400">
+				<svg xmlns="http://www.w3.org/2000/svg" width="800" height="400">
 					<rect width="800" height="400" fill="#333"/>
 					<text x="50" y="120" font-size="64" fill="white">
 						${list.objects.map((element) => element.key).toString()}
@@ -17,9 +15,8 @@ export default {
 				</svg>
 			`;
 
-
-			return new Response(await sharp(Buffer.from(svg)).png().toBuffer(), {
-				headers: { "Content-Type": "image/gif" },
+			return new Response(svg, {
+				headers: { "Content-Type": "image/png" },
 			});
 		}
 
